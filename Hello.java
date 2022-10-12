fdcb2d2eec15a8be4b73996de3313870447a7508
SAVITHA RAMESH <72896893+savram674@users.noreply.github.com>
2022-10-12 13:05:04 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Hello {
    public static void main(String[] args) {
-      System.out.rintln("Hello, World");
+      System.out.println("Hello, World");
       System.out.println("Welcome to IT A");
    }
 }
